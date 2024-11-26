c9bea646c8f358e67208a291ae1ac26ec4366946
GreatTeapot <145041658+GreatTeapot@users.noreply.github.com>
2024-11-26 09:12:43 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_vMDoojov5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: ' 7drq72tb0gh8jsqqvacui2vpbk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7drq72tb0gh8jsqqvacui2vpbk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
